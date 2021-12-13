444bb8ee5b1b607add0bee077c6183372495b0bf
Radu Chirilov <radulin4a@gmail.com>
2021-12-13 03:46:37 +0200

    Commit first plugin structure

---
+++ wp-artists.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Plugin Name: Wp Artists
+Description: WordPress plugin to treat Artists as custom Post types in order to better manage them.
+Version: 1.0
+Author: SkyMarketing
+License: MIT
+*/
